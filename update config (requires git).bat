b1e20490c5fd93828d0d1f89b63f20083639b04f
StealthSov <50644368+StealthSov@users.noreply.github.com>
2020-06-25 03:47:59 -0400

    Created auto update script

---
+++ update config (requires git).bat
@@ -0,0 +1,11 @@
+@echo off
+git clone https://github.com/StealthSov/StealthConfig --quiet>nul
+echo Copying files from git clone into cfg...
+xcopy StealthConfig %cd% /E /H /Y /Q >nul
+echo Cleaning up files (1/2)...
+rmdir /S /Q StealthConfig >nul
+echo Cleaning up files (2/2)...
+del /F /Q .gitattributes
+echo Finished! 
+echo Press any key to exit.
+pause>nul
